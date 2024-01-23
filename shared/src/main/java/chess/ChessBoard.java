dc896221a44cffdc75a9c6b47264ec8e6ff5a1d9
hDeck <hcluff2@byu.edu>
2024-01-23 00:06:55 -0700

    test not passing due to equals

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,9 @@
 package chess;
 
+import java.util.Arrays;
+
+import static chess.ChessPiece.PieceType.ROOK;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -8,6 +12,7 @@ package chess;
  */
 public class ChessBoard {
     ChessPiece[][] currBoard = new ChessPiece[9][9];
+
 
     public ChessBoard() {
         
@@ -56,7 +61,197 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
+        //clear out board
+        for(ChessPiece[] row : currBoard) {
+            for(ChessPiece spot : row) {
+                spot = null;
+            }
+        }
 
+        //refill board
+        for(int i = 1; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                ChessPiece newPiece;
+
+                //FIRST AND LAST COLS-------------------------
+                if(j == 1 || j == 8) {
+
+                    //ROOKS
+                    if(i == 1) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ROOK);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("r");
+
+                    }
+                    else if(i == 8) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ROOK);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("R");
+                    }
+
+                    //PAWNS
+                    else if(i == 2) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("p");
+                    }
+                    else if(i == 7) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("P");
+                    }
+
+                    //BLANK SPACE
+                    else {
+                        System.out.print(" ");
+                    }
+                }
+
+                //COLS 2 and 7
+                if(j==2 || j ==7) {
+                    //KNIGHTS
+                    if(i == 1) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("n");
+
+                    }
+                    else if(i == 8) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("N");
+                    }
+
+                    //PAWNS
+                    else if(i == 2) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("p");
+                    }
+                    else if(i == 7) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("P");
+                    }
+
+                    //BLANK SPACE
+                    else {
+                        System.out.print(" ");
+                    }
+                }
+
+                //COLS 3 and 6
+                if(j==3 || j ==6) {
+                    //BISHOPS
+                    if (i == 1) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("b");
+
+                    } else if (i == 8) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("B");
+                    }
+
+                    //PAWNS
+                    else if (i == 2) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("p");
+                    } else if (i == 7) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("P");
+                    }
+
+                    //BLANK SPACE
+                    else {
+                        System.out.print(" ");
+                    }
+                }
+
+                //COL 4
+                if(j==4) {
+                    //QUEENS
+                    if (i == 1) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("q");
+
+                    } else if (i == 8) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("Q");
+                    }
+
+                    //PAWNS
+                    else if (i == 2) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("p");
+                    } else if (i == 7) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("P");
+                    }
+
+                    //BLANK SPACE
+                    else {
+                        System.out.print(" ");
+                    }
+                }
+
+                //COL 5
+                if(j==5) {
+                    //KINGS
+                    if (i == 1) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("k");
+
+                    } else if (i == 8) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("K");
+                    }
+
+                    //PAWNS
+                    else if (i == 2) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("p");
+                    } else if (i == 7) {
+                        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+                        currBoard[i][j] = newPiece;
+                        System.out.print("P");
+                    }
+
+                    //BLANK SPACE
+                    else {
+                        System.out.print(" ");
+                    }
+                }
+
+                //Print seperation
+                System.out.print("|");
+
+
+
+            }
+            System.out.println();
+        }
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ChessBoard that)) return false;
+        return Arrays.equals(currBoard, that.currBoard);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(currBoard);
+    }
 }
