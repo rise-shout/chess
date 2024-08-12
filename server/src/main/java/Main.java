00fdc77a1657fbe613b56d0a4ce79167c53c5b32
rise-shout <70725776+rise-shout@users.noreply.github.com>
2024-08-11 23:01:14 -0600

    starter for local host is working

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,15 @@
 import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+        // Create a server object and run it on port 8080
+        Server server = new Server();
+        int port = server.run(8080);
+
+        System.out.println("Server is running on port: " + port);
     }
 }
