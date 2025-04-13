35def1583bf28e80c12a674307442e10997ff2e1
rise-shout <70725776+rise-shout@users.noreply.github.com>
2025-04-12 23:26:39 -0600

    added dataaccess.java and mysqldataaccess.java

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,12 @@
+package dataaccess;
+
+//import exception.ResponseException;
+import model.GameData;
+import model.UserData;
+import model.AuthData;
+
+import java.util.Collection;
+
+public interface DataAccess {
+
+}
---
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -0,0 +1,19 @@
+package dataaccess;
+
+import com.google.gson.Gson;
+//import exception.ResponseException;
+import model.GameData;
+import model.UserData;
+import model.AuthData;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.sql.*;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+
+public class MySqlDataAccess implements DataAccess {
+
+}
