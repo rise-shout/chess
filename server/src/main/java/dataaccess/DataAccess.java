ebf4bcac6a3d5b721c489700b60f582e4e1902de
rise-shout <70725776+rise-shout@users.noreply.github.com>
2025-04-13 00:00:57 -0600

    updated data access classes with more methods

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -6,7 +6,14 @@ import model.UserData;
 import model.AuthData;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface DataAccess {
+
+    int insertGame(GameData game) throws DataAccessException;
+    GameData getGameById(int gameId) throws DataAccessException;
+    void updateGame(GameData updatedGame) throws DataAccessException;
+    List<GameData> getAllGames() throws DataAccessException;
+    void clearAllGames() throws DataAccessException;
 
 }
--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -9,6 +9,7 @@ import model.AuthData;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.sql.*;
+import java.util.List;
 
 import static java.sql.Statement.RETURN_GENERATED_KEYS;
 import static java.sql.Types.NULL;
@@ -16,4 +17,28 @@ import static java.sql.Types.NULL;
 
 public class MySqlDataAccess implements DataAccess {
 
+    @Override
+    public int insertGame(GameData game) throws DataAccessException {
+        return 0;
+    }
+
+    @Override
+    public GameData getGameById(int gameId) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void updateGame(GameData updatedGame) throws DataAccessException {
+
+    }
+
+    @Override
+    public List<GameData> getAllGames() throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void clearAllGames() throws DataAccessException {
+
+    }
 }
